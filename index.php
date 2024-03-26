f55b6a826091a883af67001f280260f563d213a4
sososo0625 <kero9.7.moriya@gmail.com>
2024-03-26 13:56:17 +0900

    top編集

--- index.php
+++ index.php
@@ -16,23 +16,48 @@
 
 <body>
 
-  <div class="hero_container">
-    <img src="common/images/top_hero.jpg" alt="">
-  </div>
-
-  <div class="topic_container">
-    <div class="new_product">
-      <p>サマーシトラス</p>
+  <section id="hero_section">
+    <div class="hero_container">
+      <img src="common/images/top_hero.jpg" alt="">
     </div>
-    <div class="product_promotion">
-      <p>ドーナツのある生活</p>
-    </div>
-    <div class="item_list">
-      <p>商品一覧</p>
-    </div>
-  </div>
+  </section>
 
 
+  <section id="topic_section">
+    <div class="topic_container">
+      <div class="new_product">
+        <p>サマーシトラス</p>
+      </div>
+      <div class="product_promotion">
+        <p>ドーナツのある生活</p>
+      </div>
+      <div class="item_list">
+        <p>商品一覧</p>
+      </div>
+    </div>
+  </section>
+
+
+  <section id="introduce_section">
+    <div class="introduce_container">
+      <img src="common/images/introduce_background.jpg" alt="">
+      <h2>philosophy</h2>
+      <h3>私たちの信念</h3>
+      <p>"Creating Connections"</p>
+      <p>ドーナツでつながる</p>
+    </div>
+  </section>
+
+  <section id="ranking_section">
+    <div class="first_container">
+      <p><?php $price ?></p>
+    </div>
+    <div class="second_container"></div>
+    <div class="third_container"></div>
+    <div class="fourth_container"></div>
+    <div class="fifth_container"></div>
+    <div class="sixth_container"></div>
+  </section>
 
   <?php require 'includes/header.php'; ?>
   <br>
