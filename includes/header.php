5f54d958c9b00c97912c5f47ba1f6d9b2040ba26
okkun1024 <hirokazu.ogawa1024@gmail.com>
2024-03-26 14:08:57 +0900

    hedder追加

--- includes/header.php
+++ includes/header.php
@@ -7,36 +7,31 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="common/css/reset.css">
+  <link rel="stylesheet" href="../common/css/reset.css">
   <!-- 必要ならば下記のCSSを追加して -->
-  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="../common/css/common.css">
   <!-- タイトルここ -->
   <title>Header</title>
 </head>
 <body>
 <header>
-<div class="header_wrap">
-            <!-- 開閉式メニュー -->
-            <nav class="hum_menu">
-                <!-- チェックボックスでメニューの開閉を操作する -->
-                <input type="checkbox" class="menu_button" id="menu_button">
-                <!-- アイコンになる要素 -->
-                <label class="menu_icon" for="menu_button">
-                    <span class="menu_bg"><span class="nav_icon"></span></span>
-                </label>
-                <!-- メニュー本文 -->
-                <ul>
-                    <li><img src="" alt="logo"></li>
-                    <li><a href="">TOP</a></li>
-                    <li><a href="">商品一覧</a></li>
-                    <li><a href="">よくある質問</a></li>
-                    <li><a href="">問い合わせ</a></li>
-                    <li><a href="">当サイトのポリシー</a></li>
-                </ul>
-            </nav>
-            <h1 class="top_h1"><img src="common/image/logo.svg" class="top_img" alt="ロゴ画像"></h1>
-        </div>
+    <h1>
+        <img src="../common/images/X_icon.svg" alt="logo"></h1>
+<div class="openbtn1"><span></span><span></span><span></span></div>
+<nav id="g-nav">
+<div id="g-nav-list"><!--ナビの数が増えた場合縦スクロールするためのdiv※不要なら削除-->
+<ul>
+<li><a href="#">Top</a></li>  
+<li><a href="#">商品一覧</a></li>  
+<li><a href="#">よくある質問</a></li>  
+<li><a href="#">問い合わせ</a></li>  
+<li><a href="#">当サイトのポリシー</a></li>   
+</ul>
+</div>
+</nav>
 </header>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
+<script src="../common/js/comon.js"></script>
 </body>
 
 </html>
