ca010e9f67773001885472501c856eead9c4ee9e
okkun1024 <hirokazu.ogawa@gmail.com>
2024-03-26 20:50:10 +0900

    common.cssの修正

--- includes/header.php
+++ includes/header.php
@@ -15,16 +15,25 @@
 </head>
 <body>
 <header>
-    <div class="header_container">
-        <h1>
-            <a href="../index.php #introduce_section"><img src="../common/images/shop_logo.svg" alt="logo"></a>
-        </h1>
-        <div class="header_container_icon">
-            <a href="../login-input.php"><img src="../common/images/login_icon.svg" alt="">
-        ログイン</a>
-            <a href="../cart-show.php"><img class="cart_icon" src="../common/images/cart_icon.svg" alt="">カート</a>
+<div class="header_container">
+    <h1>
+        <a href="../index.php #introduce_section"><img src="../common/images/shop_logo.svg" alt="logo"></a>
+    </h1>
+    <div class="header_container_icon">
+        <div class="header_container_icon_login">
+            <a href="../login-input.php">
+                <img src="../common/images/login_icon.svg" alt="">
+                <span>ログイン</span>
+            </a>
+        </div>
+        <div class="header_container_icon_cart">
+            <a href="../cart-show.php">
+                <img class="cart_icon" src="../common/images/cart_icon.svg" alt="">
+                <span>カート</span>
+            </a>
         </div>
     </div>
+</div>
 <div class="openbtn1"><span></span><span></span><span></span></div>
 <nav id="g-nav">
 <div id="g-nav-list"><!--ナビの数が増えた場合縦スクロールするためのdiv※不要なら削除-->
