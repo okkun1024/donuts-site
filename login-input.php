c897abf212e929c88bcbc2c2580d9220159880ed
hallow-wa-ker <torikawadaisukitv@gmail.com>
2024-03-26 14:15:43 +0900

    sql文 顧客/商品情報つき

--- login-input.php
+++ login-input.php
@@ -9,13 +9,32 @@
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="comon/css/reset.css">
   <!-- 必要ならば下記のCSSを追加して -->
-  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="common/css/common.css">
+  <style>
+    form{
+      border: 2px solid #E8C2CA;
+      width:50%;
+    }
+    p,h1{text-align:center;}
+    input{
+      display: block;
+      margin:  auto;}
+    
+  </style>
   <!-- タイトルここ -->
-  <title>Document</title>
+  <title>ログイン-入力ページ</title>
 </head>
 
 <body>
+<h1>ログイン</h1>
 
+<form action="login-complete.php" method="post">
+<p>メールアドレス</p>
+<input type="email" name="mail" required>
+<p>パスワード</p>
+<input type="password" name="password" required>
+<input type="submit" value="ログインする">
+</form>
 </body>
 
 </html>
