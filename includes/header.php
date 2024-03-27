342f813452d4030082823f9bc9e28db5cc90a6d4
okkun1024 <hirokazu.ogawa1024@gmail.com>
2024-03-27 15:23:35 +0900

    3/27 終了時点

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../common/css/reset.css">
   <!-- 必要ならば下記のCSSを追加して -->
