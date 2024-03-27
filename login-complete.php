9cf577eda96a4b2cf602ccb424f0c567507e2899
hallow-wa-ker <torikawadaisukitv@gmail.com>
2024-03-27 11:52:27 +0900

    ログインログアウト

--- login-complete.php
+++ login-complete.php
@@ -10,12 +10,43 @@
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="common/css/reset.css">
   <!-- 必要ならば下記のCSSを追加して -->
-  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="common/css/common.css">
+  <style>
+    .log-comp h1{
+      text-align: center;
+      margin:32px 0 60px 0 ;
+      color:#7F5539;
+    } 
+    .log-comp div{
+      margin: 0 auto;
+      border: 2px solid #E8C2CA;
+      width:89%;
+      padding: 36px 0 36px 0;
+    }
+    .log-comp div p{
+      text-align: center;
+      color:#7F5539;
+    }
+    .relog{
+      width:89%;
+      margin: 0 auto;
+    }
+    .log-comp p a{
+      display: block;
+      text-align: right;
+      margin-top:24px;
+      text-decoration: underline;
+      color:#7F5539;
+    }
+  </style>
   <!-- タイトルここ -->
   <title>ログイン完了画面</title>
 </head>
 
 <body>
+<?php
+  require 'includes\header.php';
+  ?>
 <?php
 unset ($_SESSION['customer']);
 $pdo = new PDO('mysql:host=localhost;dbname=donuts;charset=utf8','donuts','password');
@@ -28,11 +59,21 @@ foreach($sql as $row ){
   'address'=>$row['address'],'mail'=>$row['mail'],'password'=>$row['password']];
 }
 if(isset($_SESSION['customer'])){
-  
+  echo'<div class=log-comp>';
+  echo '<h1>','ログイン完了','</h1>';
+  echo '<div><p>','ログインが完了しました。','</div></p>';
+  echo '<p class=relog><a href="customer-input.php">','TOPページへ戻る','</a></p>';
+}else{
+  echo'<div class=log-comp>';
+  echo '<h1>','ログイン失敗','</h1>';
+  echo '<div><p>','ログインに失敗しました。もう一度やり直してください。','</div></p>';
+  echo '<p class=relog><a href="customer-input.php">','ログインページへ戻る','</a></p>';
 }
-
 ?>
-
+</div>
+<?php
+  require 'includes\footer.php';
+?>
 </body>
 
 </html>
