a7e9193f45d7f11fac174d80f9263ff3837e9b12
okkun1024 <hirokazu.ogawa1024@gmail.com>
2024-03-28 11:24:14 +0900

    hedderとfooterを合体!

--- includes/footer.php
+++ includes/footer.php
@@ -7,9 +7,9 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../common/css/reset.css">
+  <link rel="stylesheet" href="common/css/reset.css">
   <!-- 必要ならば下記のCSSを追加して -->
-  <link rel="stylesheet" href="../common/css/common.css">
+  <link rel="stylesheet" href="common/css/common.css">
   <!-- タイトルここ -->
   <title>Footer</title>
 </head>
@@ -22,12 +22,12 @@
     <li><a href="#">当サイトのポリシー</a></li> 
   </ul>
   <ul class="footer_list_logo">
-    <li><a href="#"><img src="../common/images/instagram_icon.svg" alt=""></a></li>
-    <li class="sns_center_logo"><a href="#"><img src="../common/images/facebook_icon.svg" alt=""></a></li>
-    <li><a href="#"><img src="../common/images/X_icon.svg" alt=""></a></li> 
+    <li><a href="#"><img src="common/images/instagram_icon.svg" alt=""></a></li>
+    <li class="sns_center_logo"><a href="#"><img src="common/images/facebook_icon.svg" alt=""></a></li>
+    <li><a href="#"><img src="common/images/X_icon.svg" alt=""></a></li> 
   </ul>
   <div class="footer_shop_logo">
-  <a href="../index.php"><img src="../common/images/shop_logo.svg" alt=""></a>
+  <a href="index.php"><img src="common/images/shop_logo.svg" alt=""></a>
 
   </div>
   <p>copyright(C)c.c.donutsu All Ridhts Reserves</p>    
--- includes/header.php
+++ includes/header.php
@@ -7,9 +7,9 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../common/css/reset.css">
+  <link rel="stylesheet" href="common/css/reset.css">
   <!-- 必要ならば下記のCSSを追加して -->
-  <link rel="stylesheet" href="../common/css/common.css">
+  <link rel="stylesheet" href="common/css/common.css">
   <!-- タイトルここ -->
   <title>Header</title>
 </head>
@@ -17,8 +17,8 @@
 <header>
 <div class="header_container">
     <h1>
-        <a href="../index.php #introduce_section">
-            <img src="../common/images/shop_logo.svg" alt="logo">
+        <a href="index.php #introduce_section">
+            <img src="common/images/shop_logo.svg" alt="logo">
         </a>
     </h1>
     <div class="header_container_icon">
@@ -26,12 +26,12 @@
             <?php
             if(isset($_SESSION['customer'])){
                 echo '<a href="logout-input.php">';
-                echo '<img src="../common/images/login_icon.svg" alt="">';
+                echo '<img src="common/images/login_icon.svg" alt="">';
                 echo '<span>ログアウト</span>';
                 echo '</a>';
             }else{
                 echo '<a href="login-input.php">';
-                echo '<img src="../common/images/login_icon.svg" alt="">';
+                echo '<img src="common/images/login_icon.svg" alt="">';
                 echo '<span>ログイン</span>';
                 echo '</a>';
             }
@@ -39,8 +39,8 @@
            
         </div>
         <div class="header_container_icon_cart">
-            <a href="../cart-show.php">
-                <img class="cart_icon" src="../common/images/cart_icon.svg" alt="">
+            <a href="cart-show.php">
+                <img class="cart_icon" src="common/images/cart_icon.svg" alt="">
                 <span>カート</span>
             </a>
         </div>
@@ -50,10 +50,10 @@
 <div class="openbtn1"><span></span><span></span><span></span></div>
 <nav id="g_nav">
 <div id="g_nav_list"><!--ナビの数が増えた場合縦スクロールするためのdiv※不要なら削除-->
-<img src="../common/images/shop_logo.svg" class="nav_image">
+<img src="common/images/shop_logo.svg" class="nav_image">
 <ul>
-<li><a href="../index.php">Top</a></li>  
-<li><a href="../product.php ">商品一覧</a></li>  
+<li><a href="index.php">Top</a></li>  
+<li><a href="product.php ">商品一覧</a></li>  
 <li><a href="#">よくある質問</a></li>  
 <li><a href="#">問い合わせ</a></li>  
 <li><a href="#">当サイトのポリシー</a></li>   
@@ -72,7 +72,7 @@
 
 </header>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
-<script src="../common/js/comon.js"></script>
+<script src="common/js/comon.js"></script>
 </body>
 
 </html>
