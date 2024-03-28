8751500e0f4410b4e57343eceb450a5138ceb601
IVG-k <ktm09280201@gmail.com>
2024-03-28 13:42:02 +0900

    confirm変更

--- card-confirm.php
+++ card-confirm.php
@@ -11,11 +11,52 @@
   <!-- 必要ならば下記のCSSを追加して -->
   <link rel="stylesheet" href="common/css/common.css">
   <style>
+    h1 {
+      /* 見出しの調整・変更 */
+      margin-top: 40px;
+      margin-bottom: 60px;
+      text-align: center;
+      font-size: 20px;
+      color: #7F5539;
+    }
 
+    form {
+      /* フォームの幅調整 */
+      margin-left: 11%;
+      margin-right: 11%;
+    }
 
+    p {
+      /* 文字サイズ・外側下辺幅調整 */
+      font-size: 12px;
+      margin-bottom: 8px;
+    }
 
+    .confirmP {
+      /* 外側下辺幅・｜・内側左余白調整 */
+      margin-bottom: 24px;
+      border-left: 1px solid #7F5539;
+      padding-left: 3px;
+    }
 
+    div {
+      /* 親要素にflex*/
+      display: flex;
+      border-left: 1px solid #7F5539;
+    }
 
+    input[type=submit] {
+      /* 確認btnの調整・変更 */
+      display: block;
+      width: 60%;
+      height: 48px;
+      margin: 0 auto;
+      background-color: #7F5539;
+      color: #fff;
+      border: #fff;
+      margin-top: 100px;
+      margin-bottom: 80px;
+    }
   </style>
   <!-- タイトルここ -->
   <title>カード入力内容確認</title>
@@ -24,23 +65,28 @@
 <body>
   <img src="common/images/shop_logo.svg" alt="c.c.donuts">
   <?php
-
+  echo '<form>';
   echo '<h1>ご入力内容の確認</h1>';
-  echo '<p class="confirmP">お名前</p>';
-  echo '<p>', htmlspecialchars($_REQUEST['name']), '</p>';
-  echo '<p class="confirmP>カード会社</p>';
-  echo '<p>', htmlspecialchars($_REQUEST['card']), '</p>';
-  echo '<p class="confirmP>カード番号<p/>';
-  echo '<p>', htmlspecialchars($_REQUEST['cardNumber']), '</p>';
-  echo '<p class="confirmP>有効期限</p>';
-  echo '<p>', htmlspecialchars($_REQUEST['cardExpirationMonth']), '/', htmlspecialchars($_REQUEST['cardExpirationYear']), '</p>';
-  echo '<p class="confirmP>セキュリティーコード</p>';
+  echo '<p>お名前</p>';
+  echo '<p  class="confirmP">', htmlspecialchars($_REQUEST['name']), '</p>';
+  echo '<p>カード会社</p>';
+  echo '<p  class="confirmP">', $_REQUEST['card'], '</p>';
+  echo '<p>カード番号<p/>';
+  echo '<p  class="confirmP">', htmlspecialchars($_REQUEST['cardNumber']), '</p>';
+  echo '<p>有効期限</p>';
+  echo '<p  class="confirmP">', $_REQUEST['cardExpirationMonth'], '/', $_REQUEST['cardExpirationYear'], '</p>';
+  echo '<p>セキュリティーコード</p>';
+  echo '<div>';
   $security = mb_strlen($_REQUEST['security']);
   //forループを使用しセキュリティコードを・で出力する
-  for ($i = 0; $i <= $security; $i++) {
+  for ($i = 1; $i <= $security; $i++) {
     echo '<span>・</span>';
   }
+  echo '</div>';
 
+  echo ' <input type="submit" value="この内容で登録する">';
+
+  echo '</form>';
   ?>
 
   <?php require 'includes/footer.php'; ?>
