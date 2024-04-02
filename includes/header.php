04a34beae07aa43ec678f6dc38c1c355d351abf6
hallow-wa-ker <torikawadaisukitv@gmail.com>
2024-04-02 13:08:51 +0900

    ログインログアウト表示修正

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,4 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -25,18 +26,32 @@
             <div class="header_container_icon">
                 <div class="header_container_icon_login">
                     <?php
-                    if (isset($_SESSION['customer'])) {
-                        echo '<a href="logout-input.php">';
-                        echo '<img src="common/images/login_icon.svg" alt="">';
-                        echo '<span>ログアウト</span>';
-                        echo '</a>';
-                    } else {
-                        echo '<a href="login-input.php">';
-                        echo '<img src="common/images/login_icon.svg" alt="">';
-                        echo '<span>ログイン</span>';
-                        echo '</a>';
-                    }
+                    $url = $_SERVER['REQUEST_URI'];
+                        if (isset($_SESSION['customer'])) {
+                            echo '<a href="logout-input.php">';
+                            echo '<img src="common/images/login_icon.svg" alt="">';
+                            echo '<span>ログアウト</span>';
+                            echo '</a>';
+                        } else {
+                            echo '<a href="login-input.php">';
+                            echo '<img src="common/images/login_icon.svg" alt="">';
+                            echo '<span>ログイン</span>';
+                            echo '</a>';
+                        }
+                    
+                    // if (strstr($url, 'login-complete')) {
+                    //     echo '<a href="logout-input.php">';
+                    //     echo '<img src="common/images/login_icon.svg" alt="">';
+                    //     echo '<span>ログアウト</span>';
+                    //     echo '</a>';
+                    // } else if (strstr($url, 'logout-complete')) {
+                    //     echo '<a href="login-input.php">';
+                    //     echo '<img src="common/images/login_icon.svg" alt="">';
+                    //     echo '<span>ログイン</span>';
+                    //     echo '</a>';
+                    // }
                     ?>
+
 
                 </div>
                 <div class="header_container_icon_cart">
