6dd5352d5af4d0153a471ac6c07b315bfa9bdade
okkun1024 <hirokazu.ogawa1024@gmail.com>
2024-04-03 11:23:02 +0900

    途中

--- cart-show.php
+++ cart-show.php
@@ -1,4 +1,27 @@
 <?php require 'includes/header.php';?>
+<style>
+  div {
+    display: flex;
+  }
+</style>
+
+<div class="cart_pankuzu">
+  <span><a href="">Top</a></span>
+  <span>></span>
+  <span>カート</span>
+</div>
+
+
+<?php
+if(isset($_SESSION['customer'])){
+  echo 'ようこそ',$_SESSION['customer']['name'],'様';
+}else{
+ echo 'ようこそゲスト様';
+}
+
+
+
+?>
 
 
 
