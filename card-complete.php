7a155d28a7793b15977aafef05cb7750322bd54c
IVG-k <ktm09280201@gmail.com>
2024-04-05 14:46:02 +0900

    card情報追加

--- card-complete.php
+++ card-complete.php
@@ -17,7 +17,7 @@
 
 <body>
   <?php session_start(); ?>
-  <img src="common/images/shop_logo.svg" alt="c.c.donuts">
+  <a href="index.php"><img src="common/images/shop_logo.svg" alt="c.c.donuts"></a>
 
   <?php
 
