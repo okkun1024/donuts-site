eaeecf5883342738e0262f6da2f2754f16907119
sososo0625 <kero9.7.moriya@gmail.com>
2024-04-11 10:41:31 +0900

    a

--- index.php
+++ index.php
@@ -261,6 +261,7 @@
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script src="common/js/animation.js"></script>
   <script src="common/js/favorite.js"></script>
+  <script src="common/js/hamburger.js"></script>
 </body>
 
 </html>
